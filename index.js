d33c4ef9b72416d165720cb288fabd77d2d9d94b
jesse zenon <jesse.zenon@gmail.com>
2022-04-11 12:30:33 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", handleText);
+
+function handleText() {
+  const textString = (document.getElementById("text").textContent =
+    "This is really cool!");
+
+  console.log(textString);
+}
